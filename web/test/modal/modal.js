fd5f662d54df8fa6af0108aaad14e2d266e12a22
logongas <lorenzo.profesor@gmail.com>
2014-05-13 22:18:31 +0200

    Test de la ventana modal

---
+++ web/test/modal/modal.js
@@ -0,0 +1,27 @@
+"use strict";
+
+angular.lazy.controller("LoginController", ['$scope', 'dialog', function($scope, dialog) {
+
+        $scope.dialog.open({
+            title:"Nueva Ventana:" + Math.random()
+        });
+        
+        $scope.recibidoPrevia=$scope.dialog.data;
+
+        $scope.devolver = function() {
+                $scope.dialog.closeOK($scope.devuelto);
+        };
+
+        $scope.cancel = function() {
+            $scope.dialog.closeCancel(undefined);
+        };
+
+        $scope.newDialog = function() {
+            dialog.create(getContextPath()+"/test/modal/modal",$scope.enviado).then(function(resultado){
+                if (resultado) {
+                    $scope.recibidoSiguiente=resultado;
+                }
+            });
+        };
+
+    }]);
---
+++ web/test/modalmemory/modal.js
@@ -0,0 +1,29 @@
+"use strict";
+
+angular.lazy.controller("LoginController", ['$scope', 'dialog', '$timeout', function($scope, dialog, $timeout) {
+
+        $scope.dialog.open({
+            title: "Nueva Ventana:" + Math.random()
+        });
+
+        $scope.recibidoPrevia = $scope.dialog.data;
+
+
+
+
+
+        $scope.newDialog = function() {
+            dialog.create(getContextPath() + "/test/modalmemory/modal", 10).then(function(resultado) {
+                if (resultado === 20) {
+                    $scope.newDialog();
+                }
+            });
+        };
+
+        if ($scope.recibidoPrevia === 10) {
+            $timeout(function(){$scope.dialog.closeOK(20)}, 2000)
+        } else {
+            $scope.newDialog();
+        }
+
+    }]);

e2f7a7e99c2fc6c0c69539624e06d171c6d8da38
logongas <lorenzo.profesor@gmail.com>
2014-05-17 11:13:43 +0200

    El objeto businessMessages ahora se busca en todos los scopes

--- web/lib/ix3-angular/js/directives/ix3businessmessages.js
+++ web/lib/ix3-angular/js/directives/ix3businessmessages.js
@@ -4,13 +4,13 @@ angular.module('es.logongas.ix3').directive('ix3BusinessMessages', ['bootstrap',
         return {
             restrict: 'E',
             replace:true,
-            template: '<div data-ng-show="$parent.businessMessages.length > 0">' +
+            template: '<div data-ng-show="realScope.businessMessages.length > 0">' +
                     '       <br />' +
                     '       <div ng-class="{\'alert-error\':bootstrap.version===2,\'alert-danger\':bootstrap.version>=3}" class="alert"  >' +
-                    '           <button type="button" class="close" ng-click="$parent.businessMessages=[]">&times;</button>' +
+                    '           <button type="button" class="close" ng-click="realScope.businessMessages=[]">&times;</button>' +
                     '           <strong>Se han producido los siguientes errores:</strong>' +
                     '           <ul >' +
-                    '               <li data-ng-repeat="businessMessage in $parent.businessMessages">' +
+                    '               <li data-ng-repeat="businessMessage in realScope.businessMessages">' +
                     '                   <strong data-ng-hide="(businessMessage.propertyName == null) && (businessMessage.label == null)">{{businessMessage.label || businessMessage.propertyName}}:&nbsp;&nbsp;</strong>{{businessMessage.message}}' +
                     '               </li>' +
                     '           </ul>' +
@@ -20,6 +20,20 @@ angular.module('es.logongas.ix3').directive('ix3BusinessMessages', ['bootstrap',
             },
             link: function($scope, element, attributes) {
                 $scope.bootstrap = bootstrap;
+                
+                $scope.realScope=null;
+                var scope=$scope;
+                while (scope.$parent) {
+                    scope=scope.$parent;
+                    if (scope.hasOwnProperty('businessMessages')) {
+                        $scope.realScope=scope;
+                        break;
+                    }
+                }
+                
+                if ($scope.realScope==null) {
+                    throw Error("No existe la propiedad businessMessages en el scope.");
+                }
             }
         };
     }]);

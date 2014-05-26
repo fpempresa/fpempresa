142c35dd2d1714d4e5a8104527644a8e088c0af5
logongas <lorenzo.profesor@gmail.com>
2014-05-26 18:36:56 +0200

    Añadir al DAO de Titulado que por defecto el usuario siempre es el activo.

---
+++ src/java/es/logongas/fpempresa/persistencia/impl/dao/titulado/TituladoDAOImplHibernate.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.logongas.fpempresa.persistencia.impl.dao.titulado;
+
+import es.logongas.fpempresa.modelo.comun.Usuario;
+import es.logongas.fpempresa.modelo.titulado.Titulado;
+import es.logongas.ix3.persistence.impl.hibernate.dao.GenericDAOImplHibernate;
+import es.logongas.ix3.security.services.authentication.Principal;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author Lorenzo
+ */
+public class TituladoDAOImplHibernate extends GenericDAOImplHibernate<Titulado,Integer> {
+
+    @Autowired
+    Principal principal;    
+    
+    @Override
+    protected void preInsertBeforeTransaction(Session session, Titulado titulado) {
+        titulado.setUsuario((Usuario)principal);
+    }
+    @Override
+    protected void preUpdateBeforeTransaction(Session session, Titulado titulado) {
+        titulado.setUsuario((Usuario)principal);
+    }    
+}

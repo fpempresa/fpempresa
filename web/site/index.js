fd297790ee4f88d6fd4d677c9f4f146736bde3f3
logongas <lorenzo.profesor@gmail.com>
2014-06-04 12:26:14 +0200

    Error Falla el carrousel pq chocan  bootstrap y angularjs. Ahora el cambio de carrousel se hace desde jquery con JavaScript

--- web/site/index.js
+++ web/site/index.js
@@ -5,13 +5,3 @@ app.controller("IndexController",['$scope',function($scope) {
 }]);
 
 
-// Para el carrousel de la página principal
-$(function() {
-    var jcarousel = $('#main-carousel');
-    jcarousel.carousel({
-        interval: 5000
-    });
-    jcarousel.on('click', function(e) {
-        jcarousel.carousel('pause');
-    });
-});
--- web/site/main.js
+++ web/site/main.js
@@ -6,13 +6,29 @@ app.config(['$routeProvider', function($routeProvider) {
     }]);
 
 
-app.controller('MainController', ['$scope','goPage', function($scope,goPage) {
-        $scope.createAccount=function(tipoUsuario) {
+app.controller('MainController', ['$scope', 'goPage', function($scope, goPage) {
+        $scope.createAccount = function(tipoUsuario) {
             goPage.createAccount(tipoUsuario);
         };
-        
-        $scope.login=function() {
+
+        $scope.login = function() {
             goPage.login();
         };
-}]);
+
+
+        var jcarousel = $('#main-carousel');
+        jcarousel.carousel({
+            interval: 5000
+        });
+        jcarousel.on('click', function(e) {
+            jcarousel.carousel('pause');
+        });
+        $scope.carouselPrev = function() {
+            jcarousel.carousel('prev');
+        }
+
+        $scope.carouselNext = function() {
+            jcarousel.carousel('next');
+        }
+    }]);
 

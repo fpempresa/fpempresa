04b024cf8b50bfde4a26e15d3dc3597aedcd8a16
logongas <lorenzo.profesor@gmail.com>
2014-06-16 11:04:55 +0200

    Unificar las directivas de lov y enumerados en ix3Options

--- web/lib/ix3-angular/js/directives/ix3options.js
+++ web/lib/ix3-angular/js/directives/ix3options.js
@@ -34,12 +34,12 @@ angular.module("es.logongas.ix3").directive('ix3Options', ['remoteServiceFactory
                 return {
                     pre: function($scope, element, attributes) {
                         var filters = attributes.ix3Options;
-                        
-                        var modelProperty={
-                            name:attributes.ngModel.replace("model.", ""),
+
+                        var modelProperty = {
+                            name: attributes.ngModel.replace("model.", ""),
                             metadata: $scope.$eval("metadata[entity]." + getMetadataPropertyName(attributes.ngModel.replace("model.", ""))),
-                            scopeAccessName:attributes.ngModel
-                        }
+                            scopeAccessName: attributes.ngModel
+                        };
 
                         if (angular.isArray(modelProperty.metadata.values)) {
                             $scope.values = modelProperty.metadata.values;
@@ -50,64 +50,64 @@ angular.module("es.logongas.ix3").directive('ix3Options', ['remoteServiceFactory
                         var ngOptions;
                         if (modelProperty.metadata.type === "OBJECT") {
 
-                            if ((modelProperty.metadata.dependProperty) && (modelProperty.metadata.dependProperty.length>0)) {
+                            if ((modelProperty.metadata.dependProperty) && (modelProperty.metadata.dependProperty.length > 0)) {
                                 var remoteService = remoteServiceFactory.getRemoteService(modelProperty.metadata.className);
-                                    var parts=modelProperty.name.split(".");
-                                    parts[parts.length-1]=modelProperty.metadata.dependProperty;
-                                    var dependProperty={
-                                        name:parts.join("."),
-                                        metadata:$scope.$eval("metadata[entity]." + getMetadataPropertyName(parts.join("."))),
-                                        scopeAccessName:"model."+parts.join(".")
+                                var parts = modelProperty.name.split(".");
+                                parts[parts.length - 1] = modelProperty.metadata.dependProperty;
+                                var dependProperty = {
+                                    name: parts.join("."),
+                                    metadata: $scope.$eval("metadata[entity]." + getMetadataPropertyName(parts.join("."))),
+                                    scopeAccessName: "model." + parts.join(".")
+                                };
+                                var filterProperty = {
+                                    name: modelProperty.metadata.dependProperty, //OJO:El nombre es relativo a la propiedad principal y no desde el principio
+                                    metadata: $scope.$eval("metadata[entity]." + getMetadataPropertyName(modelProperty.name + "." + modelProperty.metadata.dependProperty))
+                                };
+                                $scope.$watch(dependProperty.scopeAccessName + "." + dependProperty.metadata.primaryKeyPropertyName, function(newValue, oldValue) {
+                                    if (newValue === oldValue) {
+                                        return;
                                     }
-                                    var filterProperty={
-                                        name:modelProperty.metadata.dependProperty, //OJO:El nombre es relativo a la propiedad principal y no desde el principio
-                                        metadata:$scope.$eval("metadata[entity]." + getMetadataPropertyName(modelProperty.name+"."+modelProperty.metadata.dependProperty)),
-                                    };
-                                    $scope.$watch(dependProperty.scopeAccessName+"."+dependProperty.metadata.primaryKeyPropertyName, function(newValue, oldValue) {
-                                        if (newValue === oldValue) {
-                                            return;
-                                        }
 
-                                        if (newValue) {
-                                            var filter = {};
-                                            filter[filterProperty.name+"."+filterProperty.metadata.primaryKeyPropertyName]=newValue;
-                                            remoteService.search(filter).then(function(values) {
-                                                $scope.values = values;
-                                                
-                                                
-                                                var exist=false;
-                                                var primaryKey=$scope.$eval( modelProperty.scopeAccessName)[modelProperty.metadata.primaryKeyPropertyName];
-                                                
-                                                for(var i=0;i<values.length;i++) {
-                                                    var value=values[i];
-                                                    if (value[modelProperty.metadata.primaryKeyPropertyName]===primaryKey) {
-                                                        exist=true;
-                                                        break;
-                                                    }
+                                    if (newValue) {
+                                        var filter = {};
+                                        filter[filterProperty.name + "." + filterProperty.metadata.primaryKeyPropertyName] = newValue;
+                                        remoteService.search(filter).then(function(values) {
+                                            $scope.values = values;
+
+
+                                            var exist = false;
+                                            var primaryKey = $scope.$eval(modelProperty.scopeAccessName)[modelProperty.metadata.primaryKeyPropertyName];
+
+                                            for (var i = 0; i < values.length; i++) {
+                                                var value = values[i];
+                                                if (value[modelProperty.metadata.primaryKeyPropertyName] === primaryKey) {
+                                                    exist = true;
+                                                    break;
                                                 }
-                                                
-                                                if (exist===false) {
-                                                    setValue($scope.$parent, modelProperty.scopeAccessName, null);
-                                                }
-                                            }, function(businessMessages) {
-                                                //Si hay un error borramos la lista y el valor dependiente
-                                                $scope.values = [];
+                                            }
+
+                                            if (exist === false) {
                                                 setValue($scope.$parent, modelProperty.scopeAccessName, null);
-                                                $scope.$parent.businessMessages = businessMessages;
-                                            });
-                                        } else {
-                                            //Si no hay valor padre, borramos la lista y el valor dependiente
+                                            }
+                                        }, function(businessMessages) {
+                                            //Si hay un error borramos la lista y el valor dependiente
                                             $scope.values = [];
                                             setValue($scope.$parent, modelProperty.scopeAccessName, null);
-                                        }
-                                    });
+                                            $scope.$parent.businessMessages = businessMessages;
+                                        });
+                                    } else {
+                                        //Si no hay valor padre, borramos la lista y el valor dependiente
+                                        $scope.values = [];
+                                        setValue($scope.$parent, modelProperty.scopeAccessName, null);
+                                    }
+                                });
                             }
 
-                           
-                            if ((filters) && (filters.trim()!=="")) {
-                                ngOptions="value as value.toString for value in values | " + filters + " track by value." + modelProperty.metadata.primaryKeyPropertyName + "";
+
+                            if ((filters) && (filters.trim() !== "")) {
+                                ngOptions = "value as value.toString for value in values | " + filters + " track by value." + modelProperty.metadata.primaryKeyPropertyName + "";
                             } else {
-                                ngOptions ="value as value.toString for value in values track by value." + modelProperty.metadata.primaryKeyPropertyName + "";
+                                ngOptions = "value as value.toString for value in values track by value." + modelProperty.metadata.primaryKeyPropertyName + "";
                             }
 
                         } else {
@@ -119,7 +119,7 @@ angular.module("es.logongas.ix3").directive('ix3Options', ['remoteServiceFactory
                     },
                     post: function($scope, element, attributes) {
                     }
-                }
+                };
             }
-        }
+        };
     }]);

61f1854a13ed58cd9b14388c325ff179dacff1f8
logongas <lorenzo.profesor@gmail.com>
2014-12-07 00:36:37 +0100

    Habilitar el navegar con Scroll y la función navigateWithScroll

--- web/site/app-run.js
+++ web/site/app-run.js
@@ -1,14 +1,46 @@
 "use strict";
 
 
-app.run(['$rootScope','routeScroll', function ($rootScope,routeScroll) {
-    //Permitirmos que se pueda poner en la ruta el parámetro "scrollTo" 
-    //para que al llegar a una ruta se mueva hasta el elemento.
-    routeScroll.enable("scrollTo");
-    
-    
+app.run(['$rootScope','$route', 'routeScroll', function ($rootScope, $route, routeScroll) {
     //Para que desde el HTML se pueda acceder al contextPath
     $rootScope.getContextPath = getContextPath;
-    
+
+}]);
+
+
+app.run(['$rootScope','$location', 'routeScroll','animateScroll', function ($rootScope, $location, routeScroll, animateScroll) {
+    //Permitirmos que se pueda poner en la ruta el parámetro "scrollTo" 
+    //para que al llegar a una ruta se mueva hasta el elemento.
+    routeScroll.enable();
+
+    //Permite navegar a una ruta y a un Scroll dentro de ella
+    //sirve pq si el usuario hace Scroll, la ruta no cambia
+    //pero al volver a navegar hay que volver a moverse hasta el scroll.
+    $rootScope.navigateWithScroll = function (page, scroll) {  
+        var url;
+        if (scroll) {
+            url=page+"?"+routeScroll.getParamName()+"="+scroll;
+        } else {
+            url=page;
+        }
+        
+        if ($location.url()===url) {
+            if (scroll) {
+                //Vamos a la misma URL pero como hay Scroll
+                //puede que el usuario se haya desplazado 
+                //y no estemos en el lugar al que queremos ir
+                //por eso hay qye volver a hacer el Scroll
+                animateScroll.toElement(scroll);
+            } else {
+                //si no hay scroll
+                //y queremos ir a la misma URL entonces
+                //seguro que no hay que hacer nada de nada
+            }
+        } else {
+            $location.url(url);
+        }
+    };
+
+
 }]);
 

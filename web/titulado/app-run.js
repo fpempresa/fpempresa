20ecbd992ed6f4bd4ce21fe2ac1e3945c7830e0e
logongas <lorenzo.profesor@gmail.com>
2014-12-15 07:55:23 +0100

    Cargar el usuario inicialmente en el "$rootScope.user"

--- web/titulado/app-run.js
+++ web/titulado/app-run.js
@@ -2,9 +2,16 @@
 
 
 app.run(['$rootScope', function ($rootScope) {
-    //Para que desde el HTML se pueda acceder al contextPath
-    $rootScope.getContextPath = getContextPath;
+        //Para que desde el HTML se pueda acceder al contextPath
+        $rootScope.getContextPath = getContextPath;
 
-}]);
+        if (user) {
+            $rootScope.user = user;
+        } else {
+            alert("No has iniciado sesión o tu sesión ha caducado");
+            window.location.href = getContextPath() + "/site/index.html";            
+        }
+
+    }]);
 
 

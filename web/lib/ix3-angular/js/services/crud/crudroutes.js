30f752cd194913f9959b2cd2454ce967e2513fb0
logongas <lorenzo.profesor@gmail.com>
2014-12-22 10:52:21 +0100

    Reorganizar el código de CRUDRoutes y permitir definir la ruta como queramos

--- web/lib/ix3-angular/js/services/crud/crudroutes.js
+++ web/lib/ix3-angular/js/services/crud/crudroutes.js
@@ -6,178 +6,207 @@
     function CRUDRoutes($routeProvider) {
 
         this.addAllRoutes = function (entity, expand) {
-            this.addListRoute(entity, expand);
+            //Al ser todas las rutas no permitimos la variable "route" ya que tendría valores distintos para cada una
+            this.addSearchRoute(entity, expand);
             this.addNewRoute(entity, expand);
             this.addEditRoute(entity, expand);
             this.addDeleteRoute(entity, expand);
             this.addViewRoute(entity, expand);
         };
- 
 
-        this.addListRoute = function (entity, expand) {
+
+        this.addSearchRoute = function (entity, expand, route) {
 
             if (!entity) {
                 throw Error("El argumento 'entity' no puede estar vacio");
             }
 
-            var fileExtension = "html";
             var lowerEntityName = entity.toLowerCase();
-            var camelEntityName = entity.charAt(0).toLowerCase() + entity.slice(1);
             var upperCamelEntityName = entity.charAt(0).toUpperCase() + entity.slice(1);
 
-
-            $routeProvider.when('/' + lowerEntityName + '/search/:parentProperty?/:parentId?', {
-                templateUrl: lowerEntityName + '/search.' + fileExtension,
+            var path = '/' + lowerEntityName + '/search/:parentProperty?/:parentId?';
+            var definitiveRoute = {
+                templateUrl: lowerEntityName + '/search.html',
                 controller: upperCamelEntityName + 'SearchController',
-                reloadOnSearch: false,
-                resolve: {
-                    controllerParams: ['$route', function ($route) {
-                            return {
-                                entity: entity,
-                                parentProperty: $route.current.params.parentProperty,
-                                parentId: $route.current.params.parentId,
-                                expand: expand
-                            };
-                        }],
-                    metadata: ['metadataEntities', function (metadataEntities) {
-                            return metadataEntities.load(entity, expand);
-                        }]
+                reloadOnSearch: false
+            }
+            var resolve = {
+                controllerParams: ['$route', function ($route) {
+                        return {
+                            entity: entity,
+                            parentProperty: $route.current.params.parentProperty,
+                            parentId: $route.current.params.parentId,
+                            expand: expand
+                        };
+                    }],
+                metadata: ['metadataEntities', function (metadataEntities) {
+                        return metadataEntities.load(entity, expand);
+                    }]
+            }
 
-                }
-            });
+            angular.extend(definitiveRoute, route);
+            definitiveRoute.resolve = definitiveRoute.resolve || {};
+            angular.extend(definitiveRoute.resolve, resolve);
+
+            $routeProvider.when(path, definitiveRoute);
         }
-        this.addNewRoute = function (entity, expand) {
+        this.addNewRoute = function (entity, expand, route) {
 
             if (!entity) {
                 throw Error("El argumento 'entity' no puede estar vacio");
             }
 
-            var fileExtension = "html";
             var lowerEntityName = entity.toLowerCase();
-            var camelEntityName = entity.charAt(0).toLowerCase() + entity.slice(1);
             var upperCamelEntityName = entity.charAt(0).toUpperCase() + entity.slice(1);
 
-            $routeProvider.when('/' + lowerEntityName + '/new/:parentProperty?/:parentId?', {
-                templateUrl: lowerEntityName + '/detail.' + fileExtension,
-                controller: upperCamelEntityName + 'NewEditController',
-                resolve: {
-                    controllerParams: ['$route', function ($route) {
-                            return {
-                                entity: entity,
-                                controllerAction: "NEW",
-                                id: null,
-                                parentProperty: $route.current.params.parentProperty,
-                                parentId: $route.current.params.parentId,
-                                expand: expand
-                            };
-                        }],
-                    metadata: ['metadataEntities', function (metadataEntities) {
-                            return metadataEntities.load(entity, expand);
-                        }]
-                }
-            });
+            var path = '/' + lowerEntityName + '/new/:parentProperty?/:parentId?';
+
+            var definitiveRoute = {
+                templateUrl: lowerEntityName + '/detail.html',
+                controller: upperCamelEntityName + 'NewEditController'
+            }
+
+            var resolve = {
+                controllerParams: ['$route', function ($route) {
+                        return {
+                            entity: entity,
+                            controllerAction: "NEW",
+                            id: null,
+                            parentProperty: $route.current.params.parentProperty,
+                            parentId: $route.current.params.parentId,
+                            expand: expand
+                        };
+                    }],
+                metadata: ['metadataEntities', function (metadataEntities) {
+                        return metadataEntities.load(entity, expand);
+                    }]
+            }
+
+            angular.extend(definitiveRoute, route);
+            definitiveRoute.resolve = definitiveRoute.resolve || {};
+            angular.extend(definitiveRoute.resolve, resolve);
+
+            $routeProvider.when(path, definitiveRoute);
 
         }
-        this.addViewRoute = function (entity, expand) {
+        this.addViewRoute = function (entity, expand, route) {
 
             if (!entity) {
                 throw Error("El argumento 'entity' no puede estar vacio");
             }
 
-            var fileExtension = "html";
             var lowerEntityName = entity.toLowerCase();
-            var camelEntityName = entity.charAt(0).toLowerCase() + entity.slice(1);
             var upperCamelEntityName = entity.charAt(0).toUpperCase() + entity.slice(1);
 
-            $routeProvider.when('/' + lowerEntityName + '/view/:id/:parentProperty?/:parentId?', {
-                templateUrl: lowerEntityName + '/detail.' + fileExtension,
-                controller: upperCamelEntityName + 'ViewController',
-                resolve: {
-                    controllerParams: ['$route', function ($route) {
-                            return {
-                                entity: entity,
-                                controllerAction: "VIEW",
-                                id: $route.current.params.id,
-                                parentProperty: $route.current.params.parentProperty,
-                                parentId: $route.current.params.parentId,
-                                expand: expand
-                            };
 
-                        }],
-                    metadata: ['metadataEntities', function (metadataEntities) {
-                            return metadataEntities.load(entity, expand);
-                        }]
-                }
-            });
+            var path = '/' + lowerEntityName + '/view/:id/:parentProperty?/:parentId?';
 
+            var definitiveRoute = {
+                templateUrl: lowerEntityName + '/detail.html',
+                controller: upperCamelEntityName + 'ViewController'
+            }
+
+            var resolve = {
+                controllerParams: ['$route', function ($route) {
+                        return {
+                            entity: entity,
+                            controllerAction: "VIEW",
+                            id: $route.current.params.id,
+                            parentProperty: $route.current.params.parentProperty,
+                            parentId: $route.current.params.parentId,
+                            expand: expand
+                        };
+                    }],
+                metadata: ['metadataEntities', function (metadataEntities) {
+                        return metadataEntities.load(entity, expand);
+                    }]
+            };
+
+            angular.extend(definitiveRoute, route);
+            definitiveRoute.resolve = definitiveRoute.resolve || {};
+            angular.extend(definitiveRoute.resolve, resolve);
+
+            $routeProvider.when(path, definitiveRoute);
         }
-        this.addEditRoute = function (entity, expand) {
+        this.addEditRoute = function (entity, expand, route) {
 
             if (!entity) {
                 throw Error("El argumento 'entity' no puede estar vacio");
             }
 
-            var fileExtension = "html";
             var lowerEntityName = entity.toLowerCase();
-            var camelEntityName = entity.charAt(0).toLowerCase() + entity.slice(1);
             var upperCamelEntityName = entity.charAt(0).toUpperCase() + entity.slice(1);
 
-            $routeProvider.when('/' + lowerEntityName + '/edit/:id/:parentProperty?/:parentId?', {
-                templateUrl: lowerEntityName + '/detail.' + fileExtension,
-                controller: upperCamelEntityName + 'NewEditController',
-                resolve: {
-                    controllerParams: ['$route', function ($route) {
-                            return {
-                                entity: entity,
-                                controllerAction: "EDIT",
-                                id: $route.current.params.id,
-                                parentProperty: $route.current.params.parentProperty,
-                                parentId: $route.current.params.parentId,
-                                expand: expand
-                            };
+            var path = '/' + lowerEntityName + '/edit/:id/:parentProperty?/:parentId?';
 
-                        }],
-                    metadata: ['metadataEntities', function (metadataEntities) {
-                            return metadataEntities.load(entity, expand);
-                        }]
-                }
-            });
+            var definitiveRoute = {
+                templateUrl: lowerEntityName + '/detail.html',
+                controller: upperCamelEntityName + 'NewEditController'
+            }
 
+            var resolve = {
+                controllerParams: ['$route', function ($route) {
+                        return {
+                            entity: entity,
+                            controllerAction: "EDIT",
+                            id: $route.current.params.id,
+                            parentProperty: $route.current.params.parentProperty,
+                            parentId: $route.current.params.parentId,
+                            expand: expand
+                        };
+                    }],
+                metadata: ['metadataEntities', function (metadataEntities) {
+                        return metadataEntities.load(entity, expand);
+                    }]
+            }
+
+            angular.extend(definitiveRoute, route);
+            definitiveRoute.resolve = definitiveRoute.resolve || {};
+            angular.extend(definitiveRoute.resolve, resolve);
+
+            $routeProvider.when(path, definitiveRoute);
         }
-        this.addDeleteRoute = function (entity, expand) {
+        this.addDeleteRoute = function (entity, expand, route) {
 
             if (!entity) {
                 throw Error("El argumento 'entity' no puede estar vacio");
             }
 
-            var fileExtension = "html";
             var lowerEntityName = entity.toLowerCase();
-            var camelEntityName = entity.charAt(0).toLowerCase() + entity.slice(1);
             var upperCamelEntityName = entity.charAt(0).toUpperCase() + entity.slice(1);
 
-            $routeProvider.when('/' + lowerEntityName + '/delete/:id/:parentProperty?/:parentId?', {
-                templateUrl: lowerEntityName + '/detail.' + fileExtension,
-                controller: upperCamelEntityName + 'DeleteController',
-                resolve: {
-                    controllerParams: ['$route', function ($route) {
-                            return {
-                                entity: entity,
-                                controllerAction: "DELETE",
-                                id: $route.current.params.id,
-                                parentProperty: $route.current.params.parentProperty,
-                                parentId: $route.current.params.parentId,
-                                expand: expand
-                            };
+            var path = '/' + lowerEntityName + '/delete/:id/:parentProperty?/:parentId?';
 
-                        }],
-                    metadata: ['metadataEntities', function (metadataEntities) {
-                            return metadataEntities.load(entity, expand);
-                        }]
-                }
-            });
+            var definitiveRoute = {
+                templateUrl: lowerEntityName + '/detail.html',
+                controller: upperCamelEntityName + 'DeleteController'
+            }
+
+
+            var resolve = {
+                controllerParams: ['$route', function ($route) {
+                        return {
+                            entity: entity,
+                            controllerAction: "DELETE",
+                            id: $route.current.params.id,
+                            parentProperty: $route.current.params.parentProperty,
+                            parentId: $route.current.params.parentId,
+                            expand: expand
+                        };
+                    }],
+                metadata: ['metadataEntities', function (metadataEntities) {
+                        return metadataEntities.load(entity, expand);
+                    }]
+            };
+
+            angular.extend(definitiveRoute, route);
+            definitiveRoute.resolve = definitiveRoute.resolve || {};
+            angular.extend(definitiveRoute.resolve, resolve);
+
+            $routeProvider.when(path, definitiveRoute);
         };
         this.$get = function () {
-            //Realmente no queremos nada aqui ero angular nos obliga.
+            //Realmente no queremos nada aqui pero angular nos obliga.
             return {};
         };
 

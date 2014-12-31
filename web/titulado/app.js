8d4704ba44f210c3e7075a7a256a8e5561f59873
logongas <lorenzo.profesor@gmail.com>
2014-12-31 18:59:38 +0100

    Quitar el módulo de "app.common" ya que no hace falta, se usa directamente el módulo "app"

--- web/titulado/app.js
+++ web/titulado/app.js
@@ -1,6 +1,6 @@
 "user strict";
 
-var app = angular.module('app', ['ui.router', 'es.logongas.ix3','common','app.common','es.logongas.ix3.datepicker.jquery','angular.filter']);
+var app = angular.module('app', ['ui.router', 'es.logongas.ix3','common','es.logongas.ix3.datepicker.jquery','angular.filter']);
 
 
 

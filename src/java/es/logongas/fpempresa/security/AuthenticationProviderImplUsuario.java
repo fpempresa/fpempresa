8d1cce1d6bac46dfffd2e5dbab26f5c56307566e
logongas <lorenzo.profesor@gmail.com>
2015-02-13 16:51:27 +0100

    Hacer uso de los Servicios en vez de de los DAO

--- src/java/es/logongas/fpempresa/security/AuthenticationProviderImplUsuario.java
+++ src/java/es/logongas/fpempresa/security/AuthenticationProviderImplUsuario.java
@@ -16,14 +16,14 @@
 package es.logongas.fpempresa.security;
 
 import es.logongas.fpempresa.modelo.comun.usuario.Usuario;
-import es.logongas.fpempresa.service.comun.usuario.UsuarioService;
+import es.logongas.fpempresa.service.comun.usuario.UsuarioCRUDService;
 import es.logongas.ix3.security.model.Identity;
 import es.logongas.ix3.core.BusinessException;
 import es.logongas.ix3.security.authentication.impl.CredentialImplLoginPassword;
 import es.logongas.ix3.security.authentication.AuthenticationProvider;
 import es.logongas.ix3.security.authentication.Credential;
 import es.logongas.ix3.security.authentication.Principal;
-import es.logongas.ix3.service.ServiceFactory;
+import es.logongas.ix3.service.CRUDServiceFactory;
 import java.io.Serializable;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -37,7 +37,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class AuthenticationProviderImplUsuario implements AuthenticationProvider {
 
     @Autowired
-    ServiceFactory serviceFactory;
+    CRUDServiceFactory crudServiceFactory;
 
     protected final Log log = LogFactory.getLog(getClass());
     
@@ -55,7 +55,7 @@ public class AuthenticationProviderImplUsuario implements AuthenticationProvider
                 return null;
             }
             
-            UsuarioService usuarioService = (UsuarioService)serviceFactory.getService(Usuario.class);
+            UsuarioCRUDService usuarioService = (UsuarioCRUDService)crudServiceFactory.getService(Usuario.class);
             Usuario usuario = usuarioService.readByNaturalKey(credentialImplLoginPassword.getLogin());            
             
             if (usuario!=null) {
@@ -79,13 +79,13 @@ public class AuthenticationProviderImplUsuario implements AuthenticationProvider
     @Override
     public Principal getPrincipalBySID(Serializable sid) throws BusinessException {
         Integer idIdentity = (Integer) sid;
-        UsuarioService usuarioService = (UsuarioService)serviceFactory.getService(Usuario.class);
+        UsuarioCRUDService usuarioService = (UsuarioCRUDService)crudServiceFactory.getService(Usuario.class);
 
         return usuarioService.read(idIdentity);
     }
 
     protected Principal getPrincipalByLogin(String login) throws BusinessException {
-        UsuarioService usuarioService = (UsuarioService)serviceFactory.getService(Usuario.class);
+        UsuarioCRUDService usuarioService = (UsuarioCRUDService)crudServiceFactory.getService(Usuario.class);
         Identity identity = usuarioService.readByNaturalKey(login);
 
         return identity;

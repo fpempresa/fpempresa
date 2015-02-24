37dc1084dc320620100f25ed97517091daaa2f50
logongas <lorenzo.profesor@gmail.com>
2015-02-24 17:02:09 +0100

    Por defecto no se añade ningún centro a la aplicación

--- src/java/es/logongas/fpempresa/database/V107__datos_centro.sql
+++ src/java/es/logongas/fpempresa/database/V107__datos_centro.sql
@@ -1,2 +1,1 @@
 INSERT INTO `centro` (`idCentro`,`nombre`) VALUES (-1,'--Pincha aqui si el centro no está en la lista--');
-INSERT INTO `centro` (`nombre`,`estadoCentro`) VALUES ('CIPFP Mislata',0);

b3446cc224bab3c48155784165dcad63d6b460be
logongas <lorenzo.profesor@gmail.com>
2015-02-27 18:44:06 +0100

    Que desde la pantalla de busqueda se pueda cambiar el estado de un usuario

--- web/administrador/views/usuario/usuario.js
+++ web/administrador/views/usuario/usuario.js
@@ -10,38 +10,47 @@ app.controller("UsuarioSearchController", ['$scope', 'genericControllerCrudList'
         genericControllerCrudList.extendScope($scope, controllerParams);
         $scope.page.pageSize = 20;
         $scope.orderby = [
-            {fieldName: 'nombre', orderDirection: 'ASC'}, 
-            {fieldName: 'ape1', orderDirection: 'ASC'}, 
+            {fieldName: 'nombre', orderDirection: 'ASC'},
+            {fieldName: 'ape1', orderDirection: 'ASC'},
             {fieldName: 'ape2', orderDirection: 'ASC'}
         ];
-        
-        $scope.filter={
-            nombre__LIKE__:null,
-            ape1__LIKE__:null,
-            ape2__LIKE__:null,
-            email__LIKE__:null
+
+        $scope.filter = {
+            nombre__LIKE__: null,
+            ape1__LIKE__: null,
+            ape2__LIKE__: null,
+            email__LIKE__: null,
+            tipoUsuario:['CENTRO','ADMINISTRADOR']
         };
-        
+
+        $scope.updateEstadoUsuario = function (idIdentity, estadoUsuario) {
+            $scope.repository.updateEstadoUsuario(idIdentity, estadoUsuario).then(function (data) {
+                $scope.search();
+            }, function (businessMessages) {
+                $scope.businessMessages = businessMessages;
+            });
+        }
+
         $scope.search();
     }]);
 
 
 app.controller("UsuarioNewEditController", ['$scope', 'genericControllerCrudDetail', 'controllerParams', function ($scope, genericControllerCrudDetail, controllerParams) {
         genericControllerCrudDetail.extendScope($scope, controllerParams);
-        
-        $scope.updateEstadoUsuario=function(estadoUsuario) {
+
+        $scope.updateEstadoUsuario = function (estadoUsuario) {
             $scope.doUpdate().then(function () {
 
-                $scope.repository.updateEstadoUsuario($scope.model.idIdentity,estadoUsuario,$scope.expand).then(function (data) {
+                $scope.repository.updateEstadoUsuario($scope.model.idIdentity, estadoUsuario, $scope.expand).then(function (data) {
                     $scope.model = data;
                     $scope.businessMessages = null;
                 }, function (businessMessages) {
                     $scope.businessMessages = businessMessages;
                 });
-                
+
             });
         }
-        
+
     }]);
 
 app.controller("UsuarioViewController", ['$scope', 'genericControllerCrudDetail', 'controllerParams', function ($scope, genericControllerCrudDetail, controllerParams) {

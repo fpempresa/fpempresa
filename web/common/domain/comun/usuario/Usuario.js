57ce3af45302fee266e96d8085f57255840bb1c9
logongas <lorenzo.profesor@gmail.com>
2015-03-09 10:46:34 +0100

    Añadir reglas de validación a la clase "Usuario"

--- web/common/domain/comun/usuario/Usuario.js
+++ web/common/domain/comun/usuario/Usuario.js
@@ -12,11 +12,37 @@ angular.module("common").config(['richDomainProvider', function (richDomain) {
                     },
                     getTipoUsuarioDescription: function () {
                         return metadataEntities.getMetadataProperty(this.$propertyPath + ".tipoUsuario").getValueDescription(this.tipoUsuario);
-                    }
+                    },
+                    $validators: [
+                        {
+                            propertyName: 'Confirmar Contraseña',
+                            message: 'No es igual a la contraseña',
+                            executeInActions: ['INSERT'],
+                            rule: function () {
+                                if (this.password === this.confirmPassword) {
+                                    return true;
+                                } else {
+                                    return false;
+                                }
+                            }
+                        },
+                        {
+                            label: "Condiciones y politicas",
+                            message: 'Debe aceptar los terminos del servicio y la política de privacidad de FPempresa',
+                            executeInActions: ['INSERT'],
+                            rule: function () {
+                                if (this.aceptarCondicionesPolitica === true) {
+                                    return true;
+                                } else {
+                                    return false;
+                                }
+                            }
+                        }
+                    ]
                 };
 
                 return function (object, propertyPath) {
-                    object.$propertyPath=propertyPath;                    
+                    object.$propertyPath = propertyPath;
                     angular.extend(object, Usuario);
                 };
             }]);

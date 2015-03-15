4aa9e2ce4ab7c1f236c36a3bd772527f73a2e50e
logongas <lorenzo.profesor@gmail.com>
2015-03-15 12:09:31 +0100

    No cargamos el usuario de un titulado si no lo pone por "expand"

--- web/common/service/titulado/TituladoService.js
+++ web/common/service/titulado/TituladoService.js
@@ -1,39 +1,84 @@
-angular.module("common").config(['serviceFactoryProvider', function (serviceFactoryProvider) {
+(function () {
 
-        serviceFactoryProvider.addExtendService("Titulado", ['service', '$q','repositoryFactory', function (service, $q, repositoryFactory) {
-                
-                service.usuarioRepository=repositoryFactory.getRepository("Usuario");
-                                
-                
-                service.get = function (id, expand) {
-                    var deferred = $q.defer();
+    "use strict";
 
-                    service.repository.get(id, expand).then(function (titulado) {
-                        
-                        service.usuarioRepository.search({"titulado.idTitulado":titulado.idTitulado}).then(function (usuarios) {
+    angular.module("common").config(['serviceFactoryProvider', function (serviceFactoryProvider) {
 
-                            if (usuarios.length>1) {
-                                throw new Error("Existe mas de un Usuario para el titulado:" + titulado.idTitulado);
+            serviceFactoryProvider.addExtendService("Titulado", ['service', '$q', 'repositoryFactory', 'langUtil', function (service, $q, repositoryFactory, langUtil) {
+
+                    service.usuarioRepository = repositoryFactory.getRepository("Usuario");
+
+                    function getExpands(expands) {
+                        var tituladoExpand = [];
+                        var usuarioExpand = [];
+                        var expandUsuario = false; //Indica si hay o no que cargar el usuario
+
+                        if (expands) {
+                            var arrExpands = langUtil.splitValues(expands, ",");
+
+                            for (var i = 0; i < arrExpands.length; i++) {
+                                var expand = arrExpands[i];
+
+                                if (expand.indexOf("usuario.")===0) {
+                                    expandUsuario = true;
+
+                                    //Le he hemos quedatos la parte de "usuario."
+                                    usuarioExpand.push(expand.substr(8));
+
+                                } else if (expand === "usuario") {
+                                    expandUsuario = true;
+                                } else {
+                                    tituladoExpand.push(expand);
+                                }
+                            }
+                        }
+
+                        return {
+                            tituladoExpand: tituladoExpand.join(","),
+                            usuarioExpand: usuarioExpand.join(","),
+                            expandUsuario: expandUsuario
+                        }
+
+                    }
+
+
+                    service.get = function (id, expand) {
+                        var deferred = $q.defer();
+
+                        var expands = getExpands(expand);
+
+                        service.repository.get(id, expands.tituladoExpand).then(function (titulado) {
+
+                            if (expands.expandUsuario) {
+                                service.usuarioRepository.search({"titulado.idTitulado": titulado.idTitulado}, undefined, expands.usuarioExpand).then(function (usuarios) {
+
+                                    if (usuarios.length > 1) {
+                                        throw new Error("Existe mas de un Usuario para el titulado:" + titulado.idTitulado);
+                                    }
+
+                                    if (usuarios.length === 1) {
+                                        titulado.usuario = usuarios[0];
+                                    }
+
+                                    deferred.resolve(titulado);
+
+                                }, function (data) {
+                                    deferred.reject(data);
+                                });
+                            } else {
+                                deferred.resolve(titulado);
                             }
 
-                            if (usuarios.length===1) {
-                                titulado.usuario=usuarios[0];
-                            }
-                            
-                            deferred.resolve(titulado);
 
                         }, function (data) {
                             deferred.reject(data);
                         });
 
-                        
-                    }, function (data) {
-                        deferred.reject(data);
-                    });
+                        return deferred.promise;
+                    };
 
-                    return deferred.promise;
-                };
-                
-            }]);
+                }]);
 
-    }]);
+        }]);
+
+})();

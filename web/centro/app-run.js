2a9f0df663cd54eadf999f16a29b59069026dc74
logongas <lorenzo.profesor@gmail.com>
2015-03-16 17:07:47 +0100

    Hacer que si el usuario de un centro no está asociado a ningún centro por defecto se muestre la centana de elegir centro

--- web/centro/app-run.js
+++ web/centro/app-run.js
@@ -1,13 +1,19 @@
 "use strict";
 
 
-app.run(['$rootScope', 'session', 'richDomain', function ($rootScope, session, richDomain) {
+app.run(['$rootScope', 'session', 'richDomain','$location', function ($rootScope, session, richDomain, $location) {
         //Guardamos la información que hemos obtenido directamente del servidor
         $rootScope.getContextPath = getContextPath;
         richDomain.extend(user);
         session.setUser(user);       
               
-        
+        //Obligamos a ir a la página de los datos del titulado si  
+        //aun no ha puesto los datos del titulado
+        $rootScope.$on("$stateChangeSuccess", function (event, toState, toParams, fromState, fromParams) {
+            if (!$rootScope.user.centro) {
+                $location.url("/pertenenciacentro");
+            }
+        });
     }]);
 
 

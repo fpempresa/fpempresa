01b7f2578c3d9e78f725e1f7c58a041398121492
logongas <lorenzo.profesor@gmail.com>
2015-03-16 17:02:21 +0100

    Sincronizar los datos del servicio "Usuario" con el usuario loggeado

--- web/common/service/comun/usuario/UsuarioService.js
+++ web/common/service/comun/usuario/UsuarioService.js
@@ -1,22 +1,26 @@
 angular.module("common").config(['serviceFactoryProvider', function (serviceFactoryProvider) {
 
-        serviceFactoryProvider.addExtendService("Usuario", ['service', '$q', function (service, $q) {
-                service.updateEstadoUsuario = function (idIdentity, estadoUsuario,expand) {
+        serviceFactoryProvider.addExtendService("Usuario", ['service', '$q','session', function (service, $q,session) {
+                service.updateEstadoUsuario = function (idIdentity, estadoUsuario, expand) {
                     var deferred = $q.defer();
 
-                    this.repository.updateEstadoUsuario(idIdentity, estadoUsuario,expand).then(function (data) {
-                        deferred.resolve(data);
+                    this.repository.updateEstadoUsuario(idIdentity, estadoUsuario, expand).then(function (usuario) {
+                        if (usuario.idIdentity===session.getUser().idIdentity) {
+                            //Si cambian los datos del usuario y es el que está logeado , actualizamos los datos del usaurio logeado
+                            session.setUser(usuario);
+                        }
+                        deferred.resolve(usuario);
                     }, function (data) {
                         deferred.reject(data);
                     });
 
                     return deferred.promise;
                 };
-                
-                service.updatePassword = function (idIdentity, currentPassword,newPassword) {
+
+                service.updatePassword = function (idIdentity, currentPassword, newPassword) {
                     var deferred = $q.defer();
 
-                    this.repository.updatePassword(idIdentity, currentPassword,newPassword).then(function () {
+                    this.repository.updatePassword(idIdentity, currentPassword, newPassword).then(function () {
                         deferred.resolve();
                     }, function (data) {
                         deferred.reject(data);
@@ -24,8 +28,41 @@ angular.module("common").config(['serviceFactoryProvider', function (serviceFact
 
                     return deferred.promise;
                 };
-                  
-                
+
+                service.update = function (id, entity, expand) {
+                    var deferred = $q.defer();
+
+                    this.repository.update(id, entity, expand).then(function (usuario) {
+                        if (usuario.idIdentity===session.getUser().idIdentity) {
+                            //Si cambian los datos del usuario y es el que está logeado , actualizamos los datos del usaurio logeado
+                            session.setUser(usuario);
+                        }
+                        deferred.resolve(usuario);
+                    }, function (businessMessages) {
+                        deferred.reject(businessMessages);
+                    });
+
+                    return deferred.promise;
+                };
+
+                this.get = function (id, expand) {
+                    var deferred = $q.defer();
+
+                    this.repository.get(id, expand).then(function (usuario) {
+                        if (usuario.idIdentity===session.getUser().idIdentity) {
+                            //Si leemos los datos del usuario del que está logeado , actualizamos los datos del usaurio logeado
+                            //Pq seguro que son mas recientes.
+                            session.setUser(usuario);
+                        }
+                        
+                        deferred.resolve(usuario);
+                    }, function (businessMessages) {
+                        deferred.reject(businessMessages);
+                    });
+
+                    return deferred.promise;
+                };
+
             }]);
 
     }]);

99f9d3fc3c3b657a512b792f4e0d2c40c5307468
logongas <lorenzo.profesor@gmail.com>
2015-03-20 21:53:56 +0100

    Al entrar en la app se va a la edición del centro.

--- web/centro/app-route.js
+++ web/centro/app-route.js
@@ -1,7 +1,7 @@
 "use strict";
 
 app.config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider) {
-        $urlRouterProvider.otherwise('/');
+        $urlRouterProvider.otherwise('/centro/edit/'+user.centro.idCentro);
         $stateProvider.state("lateralmenu",{
             templateUrl:"fragments/lateralmenu/lateralmenu.html",
             controller:"LateralMenuController"

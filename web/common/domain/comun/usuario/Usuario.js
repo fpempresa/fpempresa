11fea42e6ce7136474c9841c8add73bb4237a794
logongas <lorenzo.profesor@gmail.com>
2015-03-26 18:55:14 +0100

    Error: Usaba la columna ape1 cuando ahora se llama apellidos.

--- web/common/domain/comun/usuario/Usuario.js
+++ web/common/domain/comun/usuario/Usuario.js
@@ -5,7 +5,7 @@ angular.module("common").config(['richDomainProvider', function (richDomain) {
         richDomain.addEntityTransformer("Usuario", ['metadataEntities', function (metadataEntities) {
                 var Usuario = {
                     getNombreCompleto: function () {
-                        return this.nombre + (this.ape1 ? " " + this.ape1 : "");
+                        return this.nombre + (this.apellidos ? " " + this.apellidos : "");
                     },
                     getEstadoUsuarioDescription: function () {
                         return metadataEntities.getMetadataProperty(this.$propertyPath + ".estadoUsuario").getValueDescription(this.estadoUsuario);

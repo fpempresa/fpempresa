f9a2c76e0b48df7efcbc853ced789aa194fdaec9
logongas <lorenzo.profesor@gmail.com>
2015-03-30 10:55:30 +0200

    Nuevas tablas para la oferta

--- src/java/es/logongas/fpempresa/database/V102__esquema_empresa.sql
+++ src/java/es/logongas/fpempresa/database/V102__esquema_empresa.sql
@@ -15,3 +15,29 @@ CREATE TABLE `empresa` (
   KEY `KEY_EMPRESA_Centro` (`idCentro`),
   CONSTRAINT `KEY_EMPRESA_Centro` FOREIGN KEY (`idCentro`) REFERENCES `centro` (`idCentro`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `oferta` (
+    `idOferta` int(11) NOT NULL AUTO_INCREMENT,
+    `fecha` date DEFAULT NULL,
+    `idEmpresa` integer DEFAULT NULL,
+    `puesto` varchar(255) DEFAULT NULL,
+    `descripcion` longtext DEFAULT NULL,
+    `idMunicipio` integer DEFAULT NULL,
+    `idFamilia` integer DEFAULT NULL,
+    `tipoOferta` varchar(20) DEFAULT NULL,
+    `estadoOferta` varchar(20) DEFAULT NULL,
+    PRIMARY KEY  (`idOferta`),
+    KEY `KEY_OFERTA_EMPRESA`   (`idEmpresa`)  ,CONSTRAINT `KEY_OFERTA_EMPRESA`   FOREIGN KEY (`idEmpresa`)   REFERENCES `empresa`   (`idEmpresa`),
+    KEY `KEY_OFERTA_MUNICIPIO` (`idMunicipio`),CONSTRAINT `KEY_OFERTA_MUNICIPIO` FOREIGN KEY (`idMunicipio`) REFERENCES `municipio` (`idMunicipio`),
+    KEY `KEY_OFERTA_FAMILIA`   (`idFamilia`)  ,CONSTRAINT `KEY_OFERTA_FAMILIA`   FOREIGN KEY (`idFamilia`)   REFERENCES `familia`   (`idFamilia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ofertaciclo` (
+    `idOfertaCiclo` int(11) NOT NULL AUTO_INCREMENT,
+    `idOferta` integer not null,
+    `idCiclo` integer not null,
+    PRIMARY KEY  (`idOfertaCiclo`),
+    KEY `KEY_OFERTACICLO_OFERTA`(`idOferta`),CONSTRAINT `KEY_OFERTACICLO_OFERTA` FOREIGN KEY (`idOferta`) REFERENCES `oferta`(`idOferta`),
+    KEY `KEY_OFERTACICLO_CICLO` (`idCiclo`) ,CONSTRAINT `KEY_OFERTACICLO_CICLO`  FOREIGN KEY (`idCiclo`)  REFERENCES `ciclo` (`idCiclo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;

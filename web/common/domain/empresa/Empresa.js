21cdeafb346d3d626ba65fa5abd123699a34f452
logongas <lorenzo.profesor@gmail.com>
2015-05-20 11:34:51 +0200

    Error Se enriquecia la empresa pero se había puesto el "Centro"

--- web/common/domain/empresa/Empresa.js
+++ web/common/domain/empresa/Empresa.js
@@ -2,12 +2,12 @@
 
 angular.module("common").config(['richDomainProvider', function (richDomain) {
 
-        richDomain.addEntityTransformer("Centro", ['metadataEntities', function (metadataEntities) {
+        richDomain.addEntityTransformer("Empresa", ['metadataEntities', function (metadataEntities) {
 
                 return function (object, propertyPath) {
                     object.toString=function() {
                         return this.nombreComercial;
-                    }
+                    };
                 };
             }]);
 

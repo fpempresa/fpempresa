44ef676c4fd3f0c1df545e37b9bcc74d1c4cfde0
logongas <lorenzo.profesor@gmail.com>
2015-10-17 17:43:08 +0200

    Al insertar el usuario establecemos a que grupos de permisos pertenece cada tipo de usuario.

--- src/java/es/logongas/fpempresa/service/comun/usuario/impl/UsuarioCRUDServiceImpl.java
+++ src/java/es/logongas/fpempresa/service/comun/usuario/impl/UsuarioCRUDServiceImpl.java
@@ -25,14 +25,17 @@ import es.logongas.fpempresa.service.comun.usuario.UsuarioCRUDService;
 import es.logongas.fpempresa.service.mail.MailService;
 import es.logongas.ix3.core.BusinessException;
 import es.logongas.ix3.dao.Filter;
+import es.logongas.ix3.dao.GenericDAO;
+import es.logongas.ix3.security.model.Group;
+import es.logongas.ix3.security.model.GroupMember;
 import es.logongas.ix3.service.ParameterSearch;
 import es.logongas.ix3.service.impl.CRUDServiceImpl;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Set;
 import org.apache.commons.io.IOUtils;
 import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -90,19 +93,23 @@ public class UsuarioCRUDServiceImpl extends CRUDServiceImpl<Usuario, Integer> im
     public void insert(Usuario usuario) throws BusinessException {
 
         InputStream inputStream = UsuarioCRUDServiceImpl.class.getResourceAsStream("fotoDefecto.png");
-        
+
         try {
             byte[] foto=IOUtils.toByteArray(inputStream);
             usuario.setFoto(foto);
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
-        
+
         usuario.setPassword(getEncryptedPasswordFromPlainPassword(usuario.getPassword()));
         usuario.setValidadoEmail(false);
         usuario.setClaveValidacionEmail(SecureKeyGenerator.getSecureKey());
         sendMailValidacionEMail(usuario);
+        usuario.setMemberOf(getMembersOf(usuario));
         getUsuarioDAO().insert(usuario);
+        
+        
+        //La ponemos siemrpe a null una vez insertada para que nunca se pueda ver desde "fuera"
         usuario.setPassword(null);
     }
 
@@ -129,8 +136,8 @@ public class UsuarioCRUDServiceImpl extends CRUDServiceImpl<Usuario, Integer> im
                     usuario.setEstadoUsuario(EstadoUsuario.PENDIENTE_ACEPTACION);
                 }
             }
-        }        
-        
+        }
+
         
         //REGLA NEGOCIO:Si cambiamos el EMail hay que volver a verificar la nueva dirección
         if (!usuarioOriginal.getEmail()
@@ -157,7 +164,7 @@ public class UsuarioCRUDServiceImpl extends CRUDServiceImpl<Usuario, Integer> im
         List<Filter> filters=new ArrayList<>();
         filters.add(new Filter("titulado.idTitulado", idTitulado));
         List<Usuario> usuarios=this.getDAO().search(filters);
-        
+
         if (usuarios.size()==1) {
             return usuarios.get(0);
         } else if (usuarios.size()==0) {
@@ -165,11 +172,67 @@ public class UsuarioCRUDServiceImpl extends CRUDServiceImpl<Usuario, Integer> im
         } else {
             throw new RuntimeException("La consulta retornó mas de un elemento:" + usuarios.size());
         }
-        
+
     };
-    
+
     private void sendMailValidacionEMail(Usuario usuario) {
         //Enviar el Mail de Verificación
 
     }
+
+    private Set<GroupMember> getMembersOf(Usuario usuario) {
+        try {
+            Set<GroupMember> groupMembers = new HashSet<>();
+            GenericDAO<Group, Integer> groupDAO = daoFactory.getDAO(Group.class);
+
+            String GROUP_USUARIOS_NAME = "GUsuarios";
+            Group usuarios = groupDAO.readByNaturalKey(GROUP_USUARIOS_NAME);
+            if (usuarios == null) {
+                throw new RuntimeException("No existe el grupo " + GROUP_USUARIOS_NAME);
+            }
+            groupMembers.add(new GroupMember(0, usuarios, usuario, 0));
+
+            switch (usuario.getTipoUsuario()) {
+                case ADMINISTRADOR:
+                    String GROUP_ADMINISTRADORES_NAME = "GAdministradores";
+                    Group administradores = groupDAO.readByNaturalKey(GROUP_ADMINISTRADORES_NAME);
+                    if (administradores == null) {
+                        throw new RuntimeException("No existe el grupo " + GROUP_ADMINISTRADORES_NAME);
+                    }
+                    groupMembers.add(new GroupMember(0, administradores, usuario, 0));
+                    break;
+                case CENTRO:
+                    String GROUP_CENTROS_NAME = "GCentros";
+                    Group centros = groupDAO.readByNaturalKey(GROUP_CENTROS_NAME);
+                    if (centros == null) {
+                        throw new RuntimeException("No existe el grupo " + GROUP_CENTROS_NAME);
+                    }
+                    groupMembers.add(new GroupMember(0, centros, usuario, 0));
+                    break;
+                case EMPRESA:
+                    String GROUP_EMPRESAS_NAME = "GEmpresas";
+                    Group empresas = groupDAO.readByNaturalKey(GROUP_EMPRESAS_NAME);
+                    if (empresas == null) {
+                        throw new RuntimeException("No existe el grupo " + GROUP_EMPRESAS_NAME);
+                    }
+                    groupMembers.add(new GroupMember(0, empresas, usuario, 0));
+                    break;
+                case TITULADO:
+                     String GROUP_TITULADOS_NAME = "GTitulados";
+                    Group titulados = groupDAO.readByNaturalKey(GROUP_TITULADOS_NAME);
+                    if (titulados == null) {
+                        throw new RuntimeException("No existe el grupo " + GROUP_TITULADOS_NAME);
+                    }
+                    groupMembers.add(new GroupMember(0, titulados, usuario, 0));
+                    break;
+                default:
+                    throw new RuntimeException();
+            }
+
+            return groupMembers;
+        } catch (BusinessException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
 }

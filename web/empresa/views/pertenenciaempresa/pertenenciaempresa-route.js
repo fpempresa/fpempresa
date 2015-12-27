4d06258e191c3d7527aa186306f46cf41b508e96
logongas <lorenzo.profesor@gmail.com>
2015-12-27 20:36:29 +0100

    feature(route): Quitar el código de seleccionar la emrpesa

--- web/empresa/views/pertenenciaempresa/pertenenciaempresa-route.js
+++
@@ -1,8 +0,0 @@
-app.config(['$stateProvider', 'crudRoutesProvider', function ($stateProvider, crudRoutesProvider) {
-        $stateProvider.state('lateralmenu.pertenenciaempresa', {
-            url: "/pertenenciaempresa",
-            templateUrl: 'views/pertenenciaempresa/pertenenciaempresa.html',
-            controller: 'PertenenciaEmpresaController',
-            resolve: crudRoutesProvider.getResolve("Empresa","direccion.municipio.provincia") 
-        });
-    }]);

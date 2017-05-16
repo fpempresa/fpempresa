7222ca8c5b9549f1db5b7ff8dd789ba4e4c50170
RSginer <r.solerginer@gmail.com>
2017-05-16 17:42:23 +0200

    feat(Estadisticas): grafico en frontend de total centros en el panel de centro

--- web/centro/views/main/main.js
+++ web/centro/views/main/main.js
@@ -10,15 +10,14 @@ app.config(['$stateProvider', 'crudRoutesProvider', function ($stateProvider, cr
 MainController.$inject = ['$scope', '$http', 'ix3Configuration'];
 function MainController($scope, $http, ix3Configuration) {
     $scope.businessMessages = [];
-
-
+   
     if ($scope.user && $scope.user.centro) {
         $http({
             method: "GET",
             url: ix3Configuration.server.api + "/Estadisticas/centro/" + $scope.user.centro.idCentro
         }).then(function (chartData) {
-            console.log(chartData);
             $scope.chartData = chartData.data;
+            $scope.chartData['numeroCentros'] = [{valor: chartData.data.numeroCentros}];
         }, function (businessMessages) {
             $scope.businessMessages = businessMessages;
         });

a282d05f54d762eb46422cd221f9584fd825ebc7
Lorenzo Gonzalez <logongas@gmailcom>
2018-11-10 11:37:50 +0100

    feat(#78):Nuevos ciclos de Actividades físicas deportivas

---
+++ src/java/es/logongas/fpempresa/database/V136__nuevos_ciclos_actividades_fisicas_deportivas.sql
@@ -0,0 +1,9 @@
+INSERT INTO `ciclo` (`descripcion`, `idFamilia`, `idGrado`, `idLeyEducativa`) VALUES
+	('Actividades Ecuestres', 1, 2, 2);
+
+INSERT INTO `ciclo` (`descripcion`, `idFamilia`, `idGrado`, `idLeyEducativa`) VALUES
+	('Acondicionamiento Físico', 1, 3, 2);
+
+INSERT INTO `ciclo` (`descripcion`, `idFamilia`, `idGrado`, `idLeyEducativa`) VALUES
+	('Enseñanza y Animación Sociodeportiva', 1, 3, 2);
+

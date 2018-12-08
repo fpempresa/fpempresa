9664841fe2bbe5d7c932aab44199f05e9c437346
Lorenzo Gonzalez <logongas@gmailcom>
2018-12-08 14:17:10 +0100

    feat(#84):Sistema de monitorización

---
+++ src/java/es/logongas/fpempresa/database/V137__permisos_monitor.sql
@@ -0,0 +1,3 @@
+INSERT INTO `sec_ace` 
+(`aceType`, `idPermission`, `ididentity`, `secureResourceRegExp`, `conditionalScript`, `conditionalExpression`, `priority`, `description`) VALUES 
+('Allow', 1, 31, '/monitoring', null, null, 1, 'Permitir al administrador acceder al monitor de JavaMelody');

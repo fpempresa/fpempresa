9a27885544f53f788f0f6c0c5ae5ab1b1d47519f
Lorenzo <lorenzo.profesor@gmail.com>
2022-02-12 21:29:56 +0100

    feat(#240):Nuevos ciclos de radiología y radioterapia

---
+++ src/java/es/logongas/fpempresa/database/V168__nuevos_ciclos_radiologia.sql
@@ -0,0 +1,4 @@
+INSERT INTO `ciclo` (`descripcion`, `idFamilia`, `idGrado`, `idLeyEducativa`) VALUES
+	('Imagen para el Diagnóstico y Medicina Nuclear', 19, 3, 2);
+INSERT INTO `ciclo` (`descripcion`, `idFamilia`, `idGrado`, `idLeyEducativa`) VALUES
+	('Radioterapia y Dosimetría', 19, 3, 2);

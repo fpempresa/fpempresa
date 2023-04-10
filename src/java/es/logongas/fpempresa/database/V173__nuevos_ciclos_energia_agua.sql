e2813b7ad0a26be312f659047658c1e00ab8b39c
logongas <lorenzo.profesor@gmail.com>
2023-04-11 01:01:27 +0200

    feat(#281):Nuevos ciclos de energía y agua

---
+++ src/java/es/logongas/fpempresa/database/V173__nuevos_ciclos_energia_agua.sql
@@ -0,0 +1,6 @@
+INSERT INTO `ciclo` (`descripcion`, `idFamilia`, `idGrado`, `idLeyEducativa`) VALUES
+	('Redes y Estaciones de Tratamiento de Aguas', 26, 2, 2);
+INSERT INTO `ciclo` (`descripcion`, `idFamilia`, `idGrado`, `idLeyEducativa`) VALUES
+	('Gestión del Agua', 26, 3, 2);
+INSERT INTO `ciclo` (`descripcion`, `idFamilia`, `idGrado`, `idLeyEducativa`) VALUES
+	('Auditoría energética', 26, 4, 2);

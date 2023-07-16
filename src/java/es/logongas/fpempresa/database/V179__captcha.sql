e0cbaada92b4a3141179383e2f0ec053687447a4
logongas <lorenzo.profesor@gmail.com>
2023-07-16 23:42:09 +0200

    feat(#315):Nuevo captcha si falla
    Falta la seguridad de un nuevo método

---
+++ src/java/es/logongas/fpempresa/database/V179__captcha.sql
@@ -0,0 +1,3 @@
+INSERT INTO `sec_ace` 
+(`aceType`, `idPermission`, `ididentity`, `secureResourceRegExp`, `conditionalScript`, `conditionalExpression`, `priority`, `description`) VALUES 
+('Allow', 22, 1, 'CaptchaBusinessProcess.getCaptcha', NULL, NULL, 1, NULL);

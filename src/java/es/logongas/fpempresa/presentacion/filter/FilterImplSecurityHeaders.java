1f73b1e937255bb023fb432242bdd551c1880d7b
logongas <lorenzo.profesor@gmail.com>
2024-03-05 13:45:07 +0100

    feat(#331):Establecer headers de seguridad

---
+++ src/java/es/logongas/fpempresa/presentacion/filter/FilterImplSecurityHeaders.java
@@ -0,0 +1,58 @@
+/*
+ * FPempresa Copyright (C) 2024 Lorenzo González
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.logongas.fpempresa.presentacion.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author logongas
+ */
+public class FilterImplSecurityHeaders implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        
+        
+        httpServletResponse.addHeader("Content-Security-Policy", "default-src 'self';");
+        httpServletResponse.addHeader("X-Frame-Options", "SAMEORIGIN");
+        
+        filterChain.doFilter(servletRequest, servletResponse);
+        
+    }
+
+    @Override
+    public void destroy() {
+        
+    }
+    
+}

de8d34d3824d0358a2a6c8e308776af85e626237
logongas <lorenzo.profesor@gmail.com>
2024-07-27 19:21:26 +0200

    fix(#358):No se ve el título de "Últimas ofertas"

--- web/common/presentation/service/animate-scroll.js
+++ web/common/presentation/service/animate-scroll.js
@@ -26,7 +26,7 @@
 
         $transitions.onSuccess({ },function(transition) {
             if (transition.to().scroll)  {
-                animateScroll.toElement(transition.to().scroll);
+                animateScroll.scroll(transition.to().scroll);
             }
                 
          });
@@ -43,45 +43,27 @@ AnimateScroll.$inject=['$timeout'];
 function AnimateScroll($timeout) {
 
     /**
-     * Hace un Scroll animado hasta una posición en "Y" desde la parte superior
-     * @param {type} top La opsición "Y" desde la parte superior
-     */
-    var toTop = function (top) {
-        $('html, body').stop().animate({
-            scrollTop: top
-        }, 1500, 'easeInOutExpo');
-    }
-
-    /**
      * Hace un Scroll animado hasta un elemento
      * @param {Element || String} element Elemento hasta el que se hace un Scroll. Puede ser el propio elemento o un String con el "id" del elemento
      */
-    var toElement = function (element) {
-        var offset;
-        if (typeof (element) === "string") {
-            offset = $("#" + element).offset();
-        } else {
-            offset = element.offset();
-        }
+    var scroll = function (element) {
 
-        if (offset) {
-            toTop(offset.top);
-        } else {
-            $timeout(function () {
-                if (typeof (element) === "string") {
-                    offset = $("#" + element).offset();
-                } else {
-                    offset = element.offset();
-                }
-                toTop(offset.top);
-            }, 1);
-        }
+
+        $timeout(function () {
+            if (typeof (element) === "string") {
+                element = $("#" + element);
+            }
+            
+            $('html, body').stop().animate({
+                scrollTop: element.offset().top-40
+            }, 1500, 'easeOutSine');
+        },400);
+
     }
 
 
     return {
-        toElement: toElement,
-        toTop: toTop
+        scroll: scroll
     }
 }
 angular.module("common").factory("animateScroll", AnimateScroll);

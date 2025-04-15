2db89e9e484e0b168ab8c961201cd770e7e1d96a
logongas <lorenzo.profesor@gmail.com>
2025-04-15 13:18:44 +0200

    fix(#367):No funciona empleafp.com sin el www
    Probar las redirecciones con las pruebas.
    Que los dominios se cojas de la variable "VIRTUAL_HOST" que necesita "nginxproxy"

--- src/java/es/logongas/fpempresa/presentacion/filter/FilterImplCanonicalRedirect.java
+++ src/java/es/logongas/fpempresa/presentacion/filter/FilterImplCanonicalRedirect.java
@@ -17,8 +17,8 @@
 package es.logongas.fpempresa.presentacion.filter;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -29,24 +29,40 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- *
+ * Este filtro mira si el servidor no es el canónico y lo redirige al canónico
+ * Para elo se usa la variable "VIRTUAL_HOST" que tiene la lista de servidores que soporta
+ * Y el canónico es el primero de ellos.
+ * La variable "VIRTUAL_HOST" está porque se añade desde Docker al usar "nginxproxy"
+ * Así que siempre redirige al primero de ellos
  * @author logongas
  */
 public class FilterImplCanonicalRedirect implements Filter {
 
-    private final Map<String,String> redirects=new HashMap<>();
+    String canonicalServerName;
+    List<String> redirectServerNames=new ArrayList<>();
+    private final static String ENV_NAME_VIRTUAL_HOST="VIRTUAL_HOST";
     
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        redirects.put("pruebas2.empleafp.com","pruebas.empleafp.com");
-        redirects.put("pruebas.empleafp.es","pruebas.empleafp.com"); 
-        redirects.put("pruebas.empleafp.info","pruebas.empleafp.com");   
-        redirects.put("pruebas.empleafp.net","pruebas.empleafp.com");   
-        redirects.put("pruebas.empleafp.org","pruebas.empleafp.com");   
-        redirects.put("pruebas.empleafp.eu","pruebas.empleafp.com");   
-        redirects.put("pruebas.empleafp.info","pruebas.empleafp.com");   
-        redirects.put("pruebas.empleosfp.es","pruebas.empleafp.com");   
-        redirects.put("pruebas.empleosfp.com","pruebas.empleafp.com");   
+
+        String virtualHosts = System.getProperty(ENV_NAME_VIRTUAL_HOST);
+
+        if ((virtualHosts==null) || (virtualHosts.trim().isEmpty())) {
+            virtualHosts = System.getenv(ENV_NAME_VIRTUAL_HOST);
+        }
+
+        if ((virtualHosts==null) || (virtualHosts.trim().isEmpty())) {
+            canonicalServerName=null;
+        } else {
+            String[] partes = virtualHosts.split(",");
+            for (int i=0;i<partes.length;i++) {
+                if (i==0) {
+                    canonicalServerName=partes[i];
+                } else {
+                    redirectServerNames.add(partes[i]);
+                }
+            }
+        }
     }
 
     @Override
@@ -54,14 +70,32 @@ public class FilterImplCanonicalRedirect implements Filter {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
         
-        
-        String serverName = httpServletRequest.getServerName();
+        String scheme = httpServletRequest.getScheme();             
+        String serverName = httpServletRequest.getServerName();    
+        int serverPort = httpServletRequest.getServerPort();          
+        String requestURI = httpServletRequest.getRequestURI();   
+        String queryString = httpServletRequest.getQueryString();   
+
         
         if (isRequiredRedirect(serverName)) {
-            String target = getCanonicalServerName(serverName) + httpServletRequest.getRequestURI();
-            if (httpServletRequest.getQueryString() != null) {
-                target += "?" + httpServletRequest.getQueryString();
+            StringBuilder url = new StringBuilder();
+            url.append(scheme).append("://").append(getCanonicalServerName(serverName));
+
+            if ((scheme.equals("http") && serverPort != 80) ||
+                (scheme.equals("https") && serverPort != 443)) {
+                url.append(":").append(serverPort);
             }
+
+            
+            
+            url.append(requestURI);
+            if (queryString != null) {
+                url.append("?").append(queryString);
+            }
+
+            String target = url.toString();            
+            
+            
             httpServletResponse.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
             httpServletResponse.setHeader("Location", target);
         } else {
@@ -77,20 +111,20 @@ public class FilterImplCanonicalRedirect implements Filter {
     
     
     private boolean isRequiredRedirect(String serverName) {
-        if (redirects.get(serverName)==null) {
+        if (canonicalServerName==null) {
             return false;
-        } else {
+        }
+        
+        if (redirectServerNames.contains(serverName)) {
             return true;
         }
+        
+        return false;
     }
     
     private String getCanonicalServerName(String serverName) {
-        String canonicalServerName;
-        
-        canonicalServerName=redirects.get(serverName);
-        
-        if (canonicalServerName==null) {
-            throw new RuntimeException("No existe la redirección para " + serverName);
+        if ((canonicalServerName==null) || (canonicalServerName.trim().isEmpty())) {
+            throw new RuntimeException("La variable canonicalServerName está vacía para " + serverName);
         }
         
         return canonicalServerName;
